0d91bb584ad55892a541f613c9743900c78302c5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-11 03:26:59 +0000

    check point
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.media/trunk@61214 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/media/externalization.py
+++ src/nti/contenttypes/media/externalization.py
@@ -13,9 +13,14 @@ from zope import component
 from zope import interface
 
 from nti.externalization.interfaces import IExternalObject
+from nti.externalization.interfaces import StandardExternalFields
+
 from nti.externalization.externalization import toExternalObject
 
+from .interfaces import INTIAudio
 from .interfaces import INTIVideo
+
+MIMETYPE = StandardExternalFields.MIMETYPE
 
 @interface.implementer( IExternalObject )
 class _NTIMediaRenderExternalObject(object):
@@ -24,6 +29,18 @@ class _NTIMediaRenderExternalObject(object):
         self.media = media
 
     def _do_toExternalObject( self, extDict ):
+        if MIMETYPE in extDict:
+            extDict[StandardExternalFields.CTA_MIMETYPE] = extDict[MIMETYPE]
+        extDict.pop(StandardExternalFields.CLASS, None)
+     
+        for source in extDict.get('sources') or ():
+            source.pop(MIMETYPE, None)
+            source.pop(StandardExternalFields.CLASS, None)
+        
+        for transcript in extDict.get('transcripts') or ():
+            transcript.pop(MIMETYPE, None)
+            transcript.pop(StandardExternalFields.CLASS, None)
+            
         return extDict
 
     def toExternalObject( self, *args, **kwargs ):
@@ -33,4 +50,12 @@ class _NTIMediaRenderExternalObject(object):
 
 @component.adapter( INTIVideo )
 class _NTIVideoRenderExternalObject(_NTIMediaRenderExternalObject):
+    def _do_toExternalObject( self, extDict ):
+        extDict = super(_NTIVideoRenderExternalObject, self)._do_toExternalObject(extDict)
+        if 'closed_caption' in extDict:
+            extDict['closedCaption'] = extDict['closed_caption']
+        return extDict
+
+@component.adapter( INTIAudio )
+class _NTIAudioRenderExternalObject(_NTIMediaRenderExternalObject):
     pass
