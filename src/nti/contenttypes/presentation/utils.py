40d8955e4ec743a91883a2c5f2305faa61a65599
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-13 15:32:20 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@61381 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/utils.py
+++ src/nti/contenttypes/presentation/utils.py
@@ -14,9 +14,9 @@ from nti.externalization.internalization import update_from_external_object
 
 from .internalization import course_overview_pre_hook
 
-def create_object_from_external(ext_obj):
+def create_object_from_external(ext_obj, pre_hook=course_overview_pre_hook):
 	factory = find_factory_for(ext_obj)
 	assert factory is not None, "Coult not find factory for external object"
 	result = factory()
-	update_from_external_object(result, ext_obj, pre_hook=course_overview_pre_hook)
+	update_from_external_object(result, ext_obj, pre_hook=pre_hook)
 	return result
