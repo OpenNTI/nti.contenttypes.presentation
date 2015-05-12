c361ed25dc0be401805381e830ec415d61aa4461
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-12 02:15:44 +0000

    pass notify param
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@65063 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/utils.py
+++ src/nti/contenttypes/presentation/utils.py
@@ -31,7 +31,7 @@ def prepare_json_text(s):
 	result = unicode(s, 'utf-8') if isinstance(s, bytes) else s
 	return result
 
-def create_object_from_external(ext_obj, pre_hook=pre_hook, _exec=True):
+def create_object_from_external(ext_obj, pre_hook=pre_hook, notify=True, _exec=True):
 	__traceback_info__ = ext_obj
 	## CS: We want to call prehook in case we can to update a single dict.
 	pre_hook(None, ext_obj)
@@ -41,83 +41,94 @@ def create_object_from_external(ext_obj, pre_hook=pre_hook, _exec=True):
 		assert factory is not None, "Could not find factory for external object"
 	## create and update
 	result = factory()
-	update_from_external_object(result, ext_obj, pre_hook=pre_hook)
+	update_from_external_object(result, ext_obj, notify=notify, pre_hook=pre_hook)
 	return result
 
-def create_ntiaudio_from_external(ext_obj, _exec=True):
+def create_ntiaudio_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_ntiaudio_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_ntivideo_from_external(ext_obj, _exec=True):
+def create_ntivideo_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_ntivideo_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_ntivideoref_from_external(ext_obj, _exec=True):
+def create_ntivideoref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_ntivideoref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_ntiaudioref_from_external(ext_obj, _exec=True):
+def create_ntiaudioref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_ntiaudioref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_questionref_from_external(ext_obj, _exec=True):
+def create_questionref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
 										 pre_hook=internalization_questionref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_questionsetref_from_external(ext_obj, _exec=True):
+def create_questionsetref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_questionsetref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_assignmentref_from_external(ext_obj, _exec=True):
+def create_assignmentref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
 										 pre_hook=internalization_assignmentref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_discussion_from_external(ext_obj, _exec=True):
+def create_discussion_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_discussion_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_discussionref_from_external(ext_obj, _exec=True):
+def create_discussionref_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_discussionref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_relatedwork_from_external(ext_obj, _exec=True):
+def create_relatedwork_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_relatedworkref_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_timelime_from_external(ext_obj, _exec=True):
+def create_timelime_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_ntitimeline_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_courseoverview_from_external(ext_obj, _exec=True):
+def create_courseoverview_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_courseoverview_pre_hook,
 										 _exec=_exec)
 	return result
 
-def create_lessonoverview_from_external(ext_obj, _exec=True):
+def create_lessonoverview_from_external(ext_obj, notify=True, _exec=True):
 	result = create_object_from_external(ext_obj,
+										 notify=notify,
 										 pre_hook=internalization_lessonoverview_pre_hook,
 										 _exec=_exec)
 	return result
