80f6fb32b69d5eac0c2cc2911ed20cedf0a3e30c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-30 01:47:45 +0000

    add adapters
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@77870 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/adapters.py
+++ src/nti/contenttypes/presentation/adapters.py
@@ -14,22 +14,52 @@ from zope import interface
 
 from .media import NTIAudioRef
 from .media import NTIVideoRef
+from .media import NTIAudioRollRef
+from .media import NTIVideoRollRef
 
 from .interfaces import INTIAudio
 from .interfaces import INTIVideo
 from .interfaces import INTIAudioRef
 from .interfaces import INTIVideoRef
+from .interfaces import INTIAudioRoll
+from .interfaces import INTIVideoRoll
+from .interfaces import INTIAudioRollRef
+from .interfaces import INTIVideoRollRef
 
 @component.adapter(INTIVideo)
 @interface.implementer(INTIVideoRef)
 def ntivideo_to_ntivideoref(video):
 	result = NTIVideoRef(ntiid=video.ntiid,
+						 target=video.ntiid,
 						 poster=video.title,
 						 label=video.title)
+	result.byline = video.byline
+	result.creator = video.creator
 	return result
 
 @component.adapter(INTIAudio)
 @interface.implementer(INTIAudioRef)
 def ntiaudio_to_ntiaudioref(audio):
-	result = NTIAudioRef(ntiid=audio.ntiid)
+	result = NTIAudioRef(ntiid=audio.ntiid,
+						 target=audio.ntiid)
+	result.byline = audio.byline
+	result.creator = audio.creator
+	return result
+
+@component.adapter(INTIAudioRoll)
+@interface.implementer(INTIAudioRollRef)
+def ntiaudioroll_to_ntiaudiorollref(roll):
+	result = NTIAudioRollRef(ntiid=roll.ntiid,
+							 target=roll.ntiid)
+	result.byline = roll.byline
+	result.creator = roll.creator
+	return result
+
+@component.adapter(INTIVideoRoll)
+@interface.implementer(INTIVideoRollRef)
+def ntivideoroll_to_ntivideorollref(roll):
+	result = NTIVideoRollRef(ntiid=roll.ntiid,
+							 target=roll.ntiid)
+	result.byline = roll.byline
+	result.creator = roll.creator
 	return result
