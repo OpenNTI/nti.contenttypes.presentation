bbbc89ded922ee2a0c514b6d3e0e9ce2e7d6d09d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-16 19:54:33 +0000

    don't include core metadata fields interfaces
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@83016 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/jsonschema.py
+++ src/nti/contenttypes/presentation/jsonschema.py
@@ -9,11 +9,26 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from nti.coremetadata.interfaces import ICreatedTime
+from nti.coremetadata.interfaces import ILastModified
+
+from nti.schema.interfaces import IVariant
 from nti.schema.jsonschema import JsonSchemafier
+from nti.schema.jsonschema import ui_type_from_field
 
 class BaseJsonSchemafier(JsonSchemafier):
 
     def allow_field(self, name, field):
-        if name.startswith('_'):
+        if     name.startswith('_') \
+            or name in ICreatedTime \
+            or name in ILastModified:
             return False
         return True
+
+    def ui_types_from_field(self, field):
+        ui_type, ui_base_type = super(BaseJsonSchemafier, self).ui_types_from_field(field)
+        if IVariant.providedBy(field) and not ui_base_type:
+            base_types = map(lambda x:ui_type_from_field(x)[1], field.fields)
+            if 'string' in base_types:
+                ui_base_type = 'string'
+        return ui_type, ui_base_type
