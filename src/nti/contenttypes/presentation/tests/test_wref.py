6040228bb6ebef91a1c1eea98ca2952be37f3cda
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-06 23:35:22 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@108140 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/tests/test_wref.py
+++ src/nti/contenttypes/presentation/tests/test_wref.py
@@ -7,11 +7,13 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
 from hamcrest import assert_that
 from hamcrest import instance_of
 
+import fudge
 import unittest
 
 from nti.contenttypes.presentation.group import NTICourseOverViewGroup
@@ -28,10 +30,13 @@ class TestWref(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
-    def test_adapter(self):
+    @fudge.patch('nti.contenttypes.presentation.wref.PresentationAssetWeakRef.__call__' )
+    def test_adapter(self, mock_call):
         for clazz in (NTICourseOverViewGroup, NTIRelatedWorkRef):
             asset = clazz()
             asset.ntiid = 'tag:nextthought.com,2011-10:NTI-FOO-system_19691231181640_000000'
+            mock_call.is_callable().returns( asset )
             wref = IWeakRef(asset, None)
             assert_that(wref, is_not(none()))
             assert_that(wref, instance_of(PresentationAssetWeakRef))
+            assert_that(wref(), is_(asset))
