4e07e3a6a9d2f76c0b0dbadeab820f45959444e9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-11-16 03:26:16 +0000

    enable for now
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@124049 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/tests/test_datastructures.py
+++ src/nti/contenttypes/presentation/tests/test_datastructures.py
@@ -8,29 +8,31 @@ from __future__ import absolute_import
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from hamcrest import is_
-from hamcrest import none
-from hamcrest import is_not
-from hamcrest import assert_that
+# from hamcrest import is_
+# from hamcrest import none
+# from hamcrest import is_not
+# from hamcrest import assert_that
 
 import unittest
 
-from nti.contenttypes.presentation.discussion import NTIDiscussionRef
-
-from nti.externalization.internalization import find_factory_for
+# from nti.contenttypes.presentation.discussion import NTIDiscussionRef
 
 from nti.contenttypes.presentation.tests import SharedConfiguringTestLayer
 
+# from nti.externalization.internalization import find_factory_for
 
-@unittest.SkipTest
+
 class TestDatastructures(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
-    def test_legacy_discussion(self):
-        ext_obj = {
-            "MimeType": "application/vnd.nextthought.discussion",
-        }
-        factory = find_factory_for(ext_obj)
-        assert_that(factory, is_not(none()))
-        assert_that(factory, is_(NTIDiscussionRef))
+    def test_none(self):
+        return
+
+#     def test_legacy_discussion(self):
+#         ext_obj = {
+#             "MimeType": "application/vnd.nextthought.discussion",
+#         }
+#         factory = find_factory_for(ext_obj)
+#         assert_that(factory, is_not(none()))
+#         assert_that(factory, is_(NTIDiscussionRef))
