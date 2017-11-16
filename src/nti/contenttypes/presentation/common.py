b51823d2a4e9fb8342e9bc02c310d5001e68d55a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-11-16 03:24:53 +0000

    py3 compat
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.presentation/trunk@124048 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/presentation/common.py
+++ src/nti/contenttypes/presentation/common.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import uuid
 from datetime import datetime
@@ -30,11 +29,13 @@ from nti.coremetadata.utils import make_schema as core_schema_maker
 from nti.ntiids.ntiids import make_ntiid
 from nti.ntiids.ntiids import make_specific_safe
 
+logger = __import__('logging').getLogger(__name__)
+
 
 def generate_ntiid(nttype, provider=NTI, now=None):
     now = datetime.utcnow() if now is None else now
     dstr = now.strftime("%Y%m%d%H%M%S %f")
-    rand = str(uuid.uuid4().get_time_low()).upper()
+    rand = str(uuid.uuid4().time_low).upper()
     specific = make_specific_safe(u"%s_%s_%s" % (SYSTEM_USER_NAME, dstr, rand))
     result = make_ntiid(provider=provider,
                         nttype=nttype,
@@ -43,8 +44,8 @@ def generate_ntiid(nttype, provider=NTI, now=None):
 
 
 def make_schema(schema, user=None):
-    result = core_schema_maker(schema, 
-                               user=user, 
+    result = core_schema_maker(schema,
+                               user=user,
                                maker=IPresentationAssetJsonSchemaMaker)
     return result
 
